f6d56c36b89597e66174961e6f316ff4d2acba66
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-10 17:04:51 +0000

    Add files via upload

---
+++ exercice1/script.js
@@ -0,0 +1,33 @@
+function hello() {
+    alert("Hello World!");
+  }
+  
+  setTimeout(hello, 2000);
+  
+  
+  let div = document.getElementById("container");
+  
+  let compte = 0;
+  function pHello() {
+    let newP = document.createElement("p");
+    newP.innerText = "Hello World!";
+    div.appendChild(newP);
+    compte++;
+    if (compte == 5) {
+      clearInterval(pAdd);
+    }
+  }
+  
+  setTimeout(pHello, 2000);
+  
+
+  let btn = document.getElementById("clear");
+  
+  btn.addEventListener("click", stop);
+  
+  function stop() {
+    clearInterval(pAdd);
+  }
+  
+  let pAdd = setInterval(pHello, 2000);
+  
